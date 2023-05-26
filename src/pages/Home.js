2285b1da060f3cf41e3b642cad9f167736267df9
thiago <thiago.asantos@grupomateus.com.br>
2023-05-26 14:26:05 -0300

    retirada curso

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -208,8 +208,7 @@ function Home() {
           <ul className='container__education__item'>
             <li><h3>2023-2023</h3> <img src={LogoInteractiveDesign} className='container__education__item_logo' /> Design for a Better World with Don Norman <span className='container__education__item_now'>Studying</span></li>
             <li><h3>2023-2023</h3> <img src={LogoInteractiveDesign} className='container__education__item_logo' /> Mobile User Experience (UX) Design <span className='container__education__item_now'>Studying</span></li>
-            <li><h3>2023-2023</h3> <img src={LogoEspm} className='container__education__item_logo' /> Agile Methodologies for innovation</li>
-            <li><h3>&nbsp;</h3> <img src={LogoEspm} className='container__education__item_logo' /> Project Management with PMI and PMBOK</li>
+            <li><h3>2023-2023</h3> <img src={LogoEspm} className='container__education__item_logo' /> Project Management with PMI and PMBOK</li>
             <li><h3>&nbsp;</h3> <img src={LogoEspm} className='container__education__item_logo' /> Agile and Collaborative Leadership in Market 4.0</li>
             <li><h3>&nbsp;</h3> <img src={LogoEspm} className='container__education__item_logo' /> Design UX and UI</li>
             <li><h3>&nbsp;</h3> <img src={LogoEspm} className='container__education__item_logo' /> Information Architecture in UX</li>
